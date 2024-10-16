48b51c05996922fef1be64c88e93e92292b3a56c
Alok Desai <alokdesai2004@gmail.com>
2024-10-16 22:43:39 +0530

    feat: add nfa structure

--- main.c
+++ main.c
@@ -8,7 +8,24 @@
 #include<string.h>
 
 #define PERMS 0644
+#define INPUT_SIZE 128
+#define STATE_SIZE 3
+#define ALPHABET_SIZE 2
+#define MAX_TRANSITIONS 10
+
+char input[INPUT_SIZE];
+int nfa[STATE_SIZE][ALPHABET_SIZE][MAX_TRANSITIONS];
+int final_states[STATE_SIZE];
+
+void build_nfa(){
+    
+}
+
+void read_input(){
+    printf("Enter the input string: ");
+    scanf("%s",input);
+}
 
 int main(int argc,char** argv){
-    
+    read_input();
 }
