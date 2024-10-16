ca459518766f411a1aca29d991ee410329004209
Alok Desai <alokdesai2004@gmail.com>
2024-10-16 22:38:48 +0530

    init: init files

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+
+#define PERMS 0644
+
+int main(int argc,char** argv){
+    
+}
